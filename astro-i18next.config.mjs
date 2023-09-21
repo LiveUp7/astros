0ea3f53c1a0241c7e51b07b4e16f47de5bcdc16e
z2labplus <858570636@qq.com>
2023-09-22 05:13:41 +0800

    en

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-  defaultLocale: "en",
-  locales: ["en", , "zh", "it", "hant"],
+	defaultLocale: "en",
+	locales: ["en", "it", "zh", "tw"],
 };
-
 
 /* 
 npx astro-i18next generate
